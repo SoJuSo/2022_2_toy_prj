6e8777fc8851ffba2002bf6b3436e23d8177eb16
King-Hot <asdfg7123@gmail.com>
2022-10-14 15:07:02 +0900

    make index.html file

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express =require('express');
+const path = require("path");
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send(`Hello123`);
+    res.sendFile(path.resolve(`index.html`));
 });
 
 //
