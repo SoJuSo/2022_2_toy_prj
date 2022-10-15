60d775438d8e9f43596d146a71d185e8fdbdfa3d
King-Hot <asdfg7123@gmail.com>
2022-10-15 22:44:08 +0900

    add path

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 console.log("js load");
+
+const router = async () => {
+    // 각 route의 경로와 현재 페이지 확인용 콘솔
+    const routes = [
+      { path: "/", view: () => console.log("Viewing Dashboard") },
+      { path: "/posts", view: () => console.log("Viewing Posts") },
+      { path: "/settings", view: () => console.log("Viewing Settings") },
+    ];
+  };
