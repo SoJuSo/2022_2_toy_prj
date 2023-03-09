f9801a8946729a9f36e9606539e81370efd62617
SoJuSo <asdfg7123@gmail.com>
2023-03-09 16:53:36 +0900

    prettier 적용

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use("/src", express.static(path.resolve("src")))
+app.use("/src", express.static(path.resolve("src")));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(`index.html`));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(`index.html`));
 });
 
 //
 app.listen(port, () => {
-    console.log(`서버가 실행됩니다. http://localhost:${port}`);
+  console.log(`서버가 실행됩니다. http://localhost:${port}`);
 });
--- src/header/header.js
+++ src/header/header.js
@@ -20,6 +20,23 @@ const fetchData = async () => {
   } catch (e) {
     throw e;
   }
+  try {
+    const url = "https://api.qwer.pw/request/helpful_text?apikey=guest";
+    const option = {
+      method: "get",
+    };
+    let textSource = "";
+    const response = await fetch(url, option)
+      .then((response) => response.json())
+      //   .then(data => console.log(data[1].respond))
+      .then((data) => (textSource = data[1].respond));
+    const advice = document.createElement("div");
+    const adviceText = document.createTextNode(textSource);
+    advice.appendChild(adviceText);
+    document.getElementById("header").appendChild(advice);
+  } catch (e) {
+    throw e;
+  }
 };
 
 fetchData();
