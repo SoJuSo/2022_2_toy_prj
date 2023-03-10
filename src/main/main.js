5f293712f6c009346502a3549dbdee6d230645ce
SoJuSo <asdfg7123@gmail.com>
2023-03-10 23:37:01 +0900

    중간 파일, 끝 파일에서도 삽입 확인

--- src/main/main.js
+++ src/main/main.js
@@ -1,2 +1,7 @@
 const $ = document;
 console.log("js load in main");
+
+const advice = $.createElement("div");
+const adviceText = $.createTextNode("main");
+advice.appendChild(adviceText);
+$.getElementById("main").appendChild(advice);
