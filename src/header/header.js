d226fa431be7b72729233a27ed050aee3565106d
SoJuSo <asdfg7123@gmail.com>
2023-03-21 23:37:56 +0900

    배경색 다 날아감..재정리 필요

--- src/header/header.js
+++ src/header/header.js
@@ -20,31 +20,14 @@ const fetchData = async () => {
   } catch (e) {
     throw e;
   }
-  try {
-    const url = "https://api.qwer.pw/request/helpful_text?apikey=guest";
-    const option = {
-      method: "get",
-    };
-    let textSource = "";
-    const response = await fetch(url, option)
-      .then((response) => response.json())
-      //   .then(data => console.log(data[1].respond))
-      .then((data) => (textSource = data[1].respond));
-    const advice = document.createElement("div");
-    const adviceText = document.createTextNode(textSource);
-    advice.appendChild(adviceText);
-    document.getElementById("header").appendChild(advice);
-  } catch (e) {
-    throw e;
-  }
 };
 
 // 구조를 조금 더 깔끔하게?
 console.log(new Date());
 
-const dateView = document.createElement("div");
-const dateViewText = document.createTextNode(new Date());
-dateView.appendChild(dateViewText);
-document.getElementById("header").appendChild(dateView);
+// const dateView = document.createElement("div");
+// const dateViewText = document.createTextNode(new Date());
+// dateView.appendChild(dateViewText);
+// document.getElementById("header").appendChild(dateView);
 
 fetchData();
