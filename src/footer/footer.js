1bd075adca4f14adbac5306d9ac5fc382dd13c10
SoJuSo <asdfg7123@gmail.com>
2023-08-16 01:03:04 +0900

    명언 API 주석처리

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,2 +1,1 @@
-//깃허브 주소, 로고 추가 예정 / velog tistory 고민
 console.log(`js is load in footer`);
--- src/header/header.js
+++ src/header/header.js
@@ -1,26 +1,28 @@
 const $ = document;
 console.log(`js is loaded in header`);
 
-const fetchData = async () => {
-  try {
-    const url = "https://api.adviceslip.com/advice";
-    const option = {
-      method: "get",
-    };
-    let textSource = "";
-    const response = await fetch(url, option)
-      .then((response) => response.json())
-      //   .then(data => console.log(data[1].respond))
-      .then((data) => (textSource = data.slip.advice));
-    const advice = $.createElement("div");
-    advice.id = "id_div";
-    const adviceText = $.createTextNode(textSource);
-    advice.appendChild(adviceText);
-    $.getElementById("header").appendChild(advice);
-  } catch (e) {
-    throw e;
-  }
-};
+// API 보류
+// 문제점 1. 렌더링 순서 이슈 - HTML페이지가 렌더링 된 후 렌더링돼서 어색하다.
+// 문제점 2. 그렇게 의미있는 작업인지 모르겠다.
+// const fetchData = async () => {
+//   try {
+//     const url = "https://api.adviceslip.com/advice";
+//     const option = {
+//       method: "get",
+//     };
+//     let textSource = "";
+//     const response = await fetch(url, option)
+//       .then((response) => response.json())
+//       .then((data) => (textSource = data.slip.advice));
+//     const advice = $.createElement("div");
+//     advice.id = "id_div";
+//     const adviceText = $.createTextNode(textSource);
+//     advice.appendChild(adviceText);
+//     $.getElementById("header").appendChild(advice);
+//   } catch (e) {
+//     throw e;
+//   }
+// };
 
 // 구조를 조금 더 깔끔하게?
 console.log(new Date());
