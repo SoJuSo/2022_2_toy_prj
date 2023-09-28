e160e945986495727fb62bd08d6e8158d74787c9
SoJuSo <asdfg7123@gmail.com>
2023-09-29 00:16:26 +0900

    main.js 내부 코드 변경 Feat : + 버튼 클릭시 동작하는 함수 추가

--- src/main/main.js
+++ src/main/main.js
@@ -1,38 +1,32 @@
 console.log("js load in main");
 
-let temp = 0;
-// 방법 1
-// const addListButton = $.createElement("input");
-// addListButton.type = "button";
-// addListButton.id = "addListButton";
-// addListButton.value = "+";
-
-// 방법 2
-const addListButton = $.createElement("button");
+const addListButton = $.createElement("button"); // 버튼 추가
 addListButton.id = "addListButton";
 addListButton.innerText = "+";
-addListButton.addEventListener("click", () => {
-  temp++;
-  const list = $.createElement("div");
-  list.id = "list_div";
-  const input = $.createElement("input");
-  list.appendChild(input);
-  $.getElementById("main").appendChild(list);
-
-  const del = $.createElement("button");
-  del.id = "del_button";
-  del.addEventListener("click", () => {
-    deletefunc();
-  });
-  del.innerText = "-";
-  list.appendChild(del);
-  $.getElementById("main").appendChild(list);
-});
+addListButton.onclick = "addtodo()";
 $.getElementById("main").appendChild(addListButton);
+const addListButton_add_function = $.getElementById("addListButton");
+addListButton_add_function.addEventListener("click", () => {
+  addtodo();
+});
 
-const deletefunc = () => {
-  console.log("del", temp);
-  const div = $.getElementById("list_div");
-  div.remove();
-  temp--;
+const textfield = $.createElement("input");
+textfield.type = "text";
+textfield.id = "textfield";
+textfield.placeholder = "입력";
+$.getElementById("main").appendChild(textfield);
+// $.getElementById("textfield").addEventListener("click", addtodo());
+
+//추가하는 함수 생성
+const addtodo = () => {
+  if ($.getElementById("textfield").value == false) {
+    alert("비어있습니다!!");
+    // console.log("!!");
+  }
+  // console.log("11");
+};
+
+const deletetodo = (e) => {
+  let remove = e.target.parentElement;
+  remove.remove();
 };
