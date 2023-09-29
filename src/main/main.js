c8d7dcd61c746556a61c0249c124697303286619
SoJuSo <asdfg7123@gmail.com>
2023-09-29 20:56:53 +0900

    Feat: 오늘 추가된 기능 1. 버튼 누를 시 li태그 형태로 todolist 추가 2. 입력한 시간 추가 표기 3. 체크박스 체크 시 todolist 문자열에 줄 긋기, 체크박스 체크 해제 시 줄은 다시 사라짐 4. X 버튼을 누르면 해당 todolist만 삭제됨

--- src/main/main.js
+++ src/main/main.js
@@ -33,6 +33,9 @@
 
 console.log("js load in main");
 
+//#######################################################
+/* 상단부 입력 라인*/
+//#######################################################
 //상단 입력, 추가 div로 감싸기
 const upper_div_wrapper = $.createElement("div");
 upper_div_wrapper.id = "upper_div_wrapper";
@@ -47,11 +50,13 @@ $.getElementById("upper_div_wrapper").appendChild(addListButton);
 const textfield = $.createElement("input");
 textfield.type = "text";
 textfield.id = "textfield";
-textfield.placeholder = "입력";
+textfield.placeholder = "할 일 입력";
 $.getElementById("upper_div_wrapper").appendChild(textfield);
 // $.getElementById("textfield").addEventListener("click", addtodo());
 
-//중단부 입력
+//#######################################################
+/* 중단부 리스트 */
+//#######################################################
 const middle_div_wrapper = $.createElement("div");
 middle_div_wrapper.id = "middle_div_wrapper";
 $.getElementById("main").appendChild(middle_div_wrapper);
@@ -68,21 +73,51 @@ $.getElementById("middle_div_wrapper").appendChild(ul_wrapper);
 //#######################################################
 //추가하는 함수 생성
 
-//버튼에 추가하는 이벤트
+//버튼에 이벤트 추가
 const addListButton_add_function = $.getElementById("addListButton");
 addListButton_add_function.addEventListener("click", () => {
   addtodo();
 });
+
+//임시
 const addtodo = () => {
-  if ($.getElementById("textfield").value == false) {
-    alert("비어있습니다!!");
+  const inputtext = $.getElementById("textfield");
+  if (inputtext.value == false) {
+    alert("할 일을 입력해 주세요!");
     // console.log("!!");
   } else {
+    // alert(`${inputtext}`);
+    let addlist = $.createElement("li");
+    let delete_button = $.createElement("button");
+    let checkbox = $.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    addlist.innerHTML = new Date().toLocaleString() + "\n" + inputtext.value;
+
+    $.getElementById("ul_wrapper").appendChild(addlist);
+    addlist.appendChild(delete_button);
+    addlist.appendChild(checkbox);
+
+    delete_button.addEventListener("click", deletetodo);
+    delete_button.innerText = "x";
+    // delete_button.style.fontSize = "20px";
+
+    inputtext.value = "";
+    // inputtext.focus();
+    checkbox.addEventListener("change", function () {
+      //할일 완료 후 클릭시 밑줄로 표시
+      //temp로 구현하니 역시나, 모든 속성이 temp에 따라 바뀐다.
+      //체크박스를 통해 구현해보자
+      if (checkbox.checked) {
+        addlist.style.textDecorationLine = "line-through";
+      } else {
+        addlist.style.textDecorationLine = "none";
+      }
+    });
   }
   // console.log("11");
 };
 
 const deletetodo = (e) => {
-  let remove = e.target.parentElement;
+  let remove = e.target.parentElement; //해당 element만 삭제되어야 하기 때문
   remove.remove();
 };
