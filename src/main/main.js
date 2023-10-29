1c2f3300f881be0b248cdc497b7f45e844c0c952
SoJuSo <asdfg7123@gmail.com>
2023-10-29 23:30:10 +0900

    [Comment] 사용하지 않는 주석 제거

--- src/main/main.js
+++ src/main/main.js
@@ -1,34 +1,3 @@
-// console.log("js load in main");
-
-// const addListButton = $.createElement("button"); // 버튼 추가
-// addListButton.id = "addListButton";
-// addListButton.innerText = "+";
-// $.getElementById("main").appendChild(addListButton);
-
-// const textfield = $.createElement("input");
-// textfield.type = "text";
-// textfield.id = "textfield";
-// textfield.placeholder = "입력";
-// $.getElementById("main").appendChild(textfield);
-// // $.getElementById("textfield").addEventListener("click", addtodo());
-
-// //추가하는 함수 생성
-// const addtodo = () => {
-//   if ($.getElementById("textfield").value == false) {
-//     alert("비어있습니다!!");
-//     console.log("!!");
-//   }
-//   console.log("11");
-// };
-
-// const addButton = $.getElementById("addListButton");
-// addButton.addEventListener("on", addtodo());
-
-// const deletetodo = (e) => {
-//   let remove = e.target.parentElement;
-//   remove.remove();
-// };
-
 //왜 위에 코드 실행시키면 바로 실행되는지 알아내기
 
 console.log("js load in main");
